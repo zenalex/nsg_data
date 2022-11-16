5dd29282b840c359215559ee754f63092d259643
AndyProtech <andy.protech@gmail.com>
2022-11-16 18:32:59 +0300

    fix

--- lib/helpers/double_extension.dart
+++ lib/helpers/double_extension.dart
@@ -1,8 +1,14 @@
 import 'dart:math';
+import 'package:intl/intl.dart';
 
 extension NsgDoubleExtension on double {
   double nsgRoundToDouble(int maxDecimalPlaces) {
     var m = pow(10, maxDecimalPlaces);
     return (m * this).roundToDouble() / m;
+  }
+
+  String toStringFormatted() {
+    var formatter = NumberFormat("#,###,000", "ru_RU");
+    return formatter.format(this);
   }
 }
