fae6bd49b4606d2bac36ef52013402bb62ee9b18
zenalex <zenkov@nsgsoft.ru>
2023-01-23 18:51:35 +0300

    убран экран loginSuccessful

--- lib/nsg_data_item.dart
+++ lib/nsg_data_item.dart
@@ -103,7 +103,8 @@ class NsgDataItem {
 
   ///Добавление ногого поля в объект
   ///Вызывается при инициализации
-  void addField(NsgDataField field, {bool primaryKey = false, String? presentation}) {
+  void addField(NsgDataField field,
+      {bool primaryKey = false, String? presentation}) {
     var name = field.name;
     assert(!fieldList.fields.containsKey(name));
     fieldList.fields[name] = field;
@@ -133,7 +134,12 @@ class NsgDataItem {
       return fieldValues.fields[name];
     } else {
       //Проверка на наличие поля в списке полей объекта
-      assert(fieldList.fields.containsKey(name), '!!! Не существует поля с именем: ' + name + ' в объекте: ' + typeName);
+      assert(
+          fieldList.fields.containsKey(name),
+          '!!! Не существует поля с именем: ' +
+              name +
+              ' в объекте: ' +
+              typeName);
       //Проверка не является ли поле пустым (умышленно не читалось из БД, следовательно, нельзя брать значение из него)
       assert(!fieldValues.emptyFields.contains(name));
       if (allowNullValue) return null;
@@ -144,9 +150,9 @@ class NsgDataItem {
   ///Установить значение поля
   void setFieldValue(String name, dynamic value) {
     //TODO: убрать этот метод, присваивать значения в setValue полей
-    if (!fieldList.fields.containsKey(name)) {
-      assert(fieldList.fields.containsKey(name), 'object $runtimeType does not contains field $name');
-    }
+    assert(fieldList.fields.containsKey(name),
+        'object $runtimeType does not contains field $name');
+    if (!fieldList.fields.containsKey(name)) {}
     var field = getField(name);
     if (field is NsgDataDoubleField) {
       field.setValue(fieldValues, value);
@@ -156,7 +162,8 @@ class NsgDataItem {
       value = value.value;
     } else if (value is NsgDataItem) {
       if (fieldList.fields[name] is NsgDataUntypedReferenceField) {
-        value = '${value.getFieldValue(value.primaryKeyField)}.${value.typeName}';
+        value =
+            '${value.getFieldValue(value.primaryKeyField)}.${value.typeName}';
       } else {
         value = value.getFieldValue(value.primaryKeyField);
       }
@@ -170,7 +177,9 @@ class NsgDataItem {
     } else if (name != primaryKeyField) {
       if (value is String) {
         var field = getField(name);
-        if (field is NsgDataStringField && value.length > field.maxLength && field.maxLength != 0) {
+        if (field is NsgDataStringField &&
+            value.length > field.maxLength &&
+            field.maxLength != 0) {
           value = value.toString().substring(0, field.maxLength);
         } else if (field is NsgDataDoubleField) {
           //TODO: такое впечатление, что весь это метод надо заменить на данную строку.
@@ -186,7 +195,8 @@ class NsgDataItem {
   ///Пометить поле пустым, т.е. что оно не загружалось из БД
   void setFieldEmpty(String name) {
     if (!fieldList.fields.containsKey(name)) {
-      assert(fieldList.fields.containsKey(name), 'object $runtimeType does not contains field $name');
+      assert(fieldList.fields.containsKey(name),
+          'object $runtimeType does not contains field $name');
     }
     fieldValues.setEmpty(this, name);
   }
@@ -201,7 +211,8 @@ class NsgDataItem {
     }
   }
 
-  set remoteProvider(NsgDataProvider? value) => paramList.params[_PARAM_REMOTE_PROVIDER] = value;
+  set remoteProvider(NsgDataProvider? value) =>
+      paramList.params[_PARAM_REMOTE_PROVIDER] = value;
 
   ///В случае ссылочного поля позвращает объект, на который ссылается данное поле
   T getReferent<T extends NsgDataItem?>(String name) {
@@ -232,11 +243,13 @@ class NsgDataItem {
   }
 
   ///В случае ссылочного поля позвращает объект, на который ссылается данное поле. Если поле не прочитано из БД, читает его асинхронно
-  Future<T> getReferentAsync<T extends NsgDataItem>(String name, {bool useCache = true}) async {
+  Future<T> getReferentAsync<T extends NsgDataItem>(String name,
+      {bool useCache = true}) async {
     assert(fieldValues.fields.containsKey(name));
     var field = fieldList.fields[name]!;
     assert(field is NsgDataReferenceField);
-    var dataItem = await ((field as NsgDataReferenceField).getReferentAsync(this, useCache: useCache));
+    var dataItem = await ((field as NsgDataReferenceField)
+        .getReferentAsync(this, useCache: useCache));
     return dataItem as T;
   }
 
@@ -253,7 +266,8 @@ class NsgDataItem {
   }
 
   ///Устанавливает значение ключевого поля (обычно Guid)
-  set primaryKeyField(String value) => paramList.params[_PRIMARY_KEY_FIELD] = value;
+  set primaryKeyField(String value) =>
+      paramList.params[_PRIMARY_KEY_FIELD] = value;
 
   ///Возвращает список всех полей ссылочных типов
   List<String> getAllReferenceFields() {
@@ -303,7 +317,8 @@ class NsgDataItem {
     if (storageType == NsgDataStorageType.server) {
       var p = NsgDataPost(dataItemType: runtimeType);
       p.itemsToPost = <NsgDataItem>[this];
-      var newItem = await p.postItem(loadReference: NsgDataRequest.addAllReferences(runtimeType));
+      var newItem = await p.postItem(
+          loadReference: NsgDataRequest.addAllReferences(runtimeType));
       if (newItem != null) {
         copyFieldValues(newItem);
         state = newItem.state;
@@ -386,7 +401,10 @@ class NsgDataItem {
     var list = <String>[];
     for (var fieldName in fieldList.fields.keys) {
       var field = getField(fieldName);
-      if (field.name == primaryKeyField || field is NsgDataUntypedReferenceField || field is NsgDataEnumReferenceField || field is NsgDataDateField) {
+      if (field.name == primaryKeyField ||
+          field is NsgDataUntypedReferenceField ||
+          field is NsgDataEnumReferenceField ||
+          field is NsgDataDateField) {
         continue;
       }
       list.add(fieldName);
@@ -406,13 +424,15 @@ class NsgDataItem {
     var answer = NsgValidateResult();
     for (var fieldName in fieldList.fields.keys) {
       if (isFieldRequired(fieldName)) {
-        if (fieldValues.fields[fieldName] == fieldList.fields[fieldName]!.defaultValue) {
+        if (fieldValues.fields[fieldName] ==
+            fieldList.fields[fieldName]!.defaultValue) {
           var fieldPresentation = fieldList.fields[fieldName]!.presentation;
           if (fieldPresentation.isEmpty) {
             fieldPresentation = fieldName;
           }
           answer.isValid = false;
-          answer.fieldsWithError[fieldName] = 'Не заполнено обязательное поле $fieldPresentation';
+          answer.fieldsWithError[fieldName] =
+              'Не заполнено обязательное поле $fieldPresentation';
         }
       }
     }
