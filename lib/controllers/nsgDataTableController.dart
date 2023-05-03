34c56965144bdb161f07f488913c7bf267979ce2
AndyProtech <andy.protech@gmail.com>
2023-05-03 14:54:41 +0300

    fix

--- lib/controllers/nsgDataTableController.dart
+++ lib/controllers/nsgDataTableController.dart
@@ -39,7 +39,7 @@ class NsgDataTableController<T extends NsgDataItem> extends NsgDataController<T>
     row.state = NsgDataItemState.create;
     row.storageType = controllerMode.storageType;
     row.ownerId = masterController!.selectedItem!.id;
-    //dataTable.addRow(row);
+    //dataTable.addRow(row); // FIXME не работает создание нового элемента в табличном контроллере!
     return row;
   }
 
