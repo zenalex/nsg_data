73d78931343db6b885767b83c2c661baa07b132b
Mishasik <mishelzenkov@gmail.com>
2023-06-19 19:03:11 +0300

    Update nsg_navigator.dart

--- lib/navigator/nsg_navigator.dart
+++ lib/navigator/nsg_navigator.dart
@@ -1,7 +1,19 @@
 import 'package:get/get.dart';
 
-class  NsgNavigator {
+class NsgNavigator {
   static NsgNavigator instance = NsgNavigator();
+
+  static void go(String pageName) {
+    instance.offAndToPage(pageName);
+  }
+
+  static void push(String pageName) {
+    instance.toPage(pageName);
+  }
+
+  static void pop(String pageName) {
+    instance.back();
+  }
 
   void toPage(String pageName) {
     Get.toNamed(pageName);
