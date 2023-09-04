4ff3e4cf22c777ae5372b561a9eb8c3ff1f6a616
AndyProtech <andy.protech@gmail.com>
2023-09-04 18:44:42 +0300

    nsgfutureprogress fix exception

--- lib/helpers/nsg_future_progress_exception.dart
+++ lib/helpers/nsg_future_progress_exception.dart
@@ -3,14 +3,25 @@ import 'package:nsg_controls/nsg_controls.dart';
 import 'package:nsg_controls/widgets/nsg_error_widget.dart';
 
 /// Вызов Future функции с прогресс индикатором и проверкой на ошибки
-Future nsgFutureProgressAndException({required Function() func, String? text, BuildContext? context}) async {
+Future nsgFutureProgressAndException({required Function() func, String? text, BuildContext? context, bool showProgress = true}) async {
   var progress = NsgProgressDialog(textDialog: text ?? 'Сохранение данных на сервер', context: context);
   try {
-    progress.show();
+    if (showProgress) {
+      progress.show();
+    }
     await func();
-    progress.hide();
-  } catch (ex) {
-    progress.hide();
-    await NsgErrorWidget.showError(ex as Exception);
+    if (showProgress) {
+      progress.hide();
+    }
+  } on Exception catch (ex) {
+    if (showProgress) {
+      progress.hide();
+    }
+    if (ex is Exception) {
+      await NsgErrorWidget.showError(ex as Exception);
+    } else {
+      print(ex);
+      await NsgErrorWidget.showError(ex as Exception);
+    }
   }
 }
