248c834086afd20845528e1eeb57525e384f19e7
AndyProtech <andy.protech@gmail.com>
2023-09-25 22:13:57 +0300

    remove default russian text form progressbar

--- lib/helpers/nsg_future_progress_exception.dart
+++ lib/helpers/nsg_future_progress_exception.dart
@@ -4,7 +4,7 @@ import 'package:nsg_controls/widgets/nsg_error_widget.dart';
 
 /// Вызов Future функции с прогресс индикатором и проверкой на ошибки
 Future nsgFutureProgressAndException({required Function() func, String? text, BuildContext? context, bool showProgress = true}) async {
-  var progress = NsgProgressDialog(textDialog: text ?? 'Сохранение данных на сервер', context: context);
+  var progress = NsgProgressDialog(textDialog: text ?? '', context: context);
   try {
     if (showProgress) {
       progress.show();
