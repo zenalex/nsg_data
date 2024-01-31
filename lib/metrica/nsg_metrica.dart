8997f247f129b05c20b4a254f72a66f9ab31ef93
Aleksei <zenkov@nsgsoft.ru>
2024-01-31 12:56:41 +0300

    NsgMetrica improve

--- lib/metrica/nsg_metrica.dart
+++ lib/metrica/nsg_metrica.dart
@@ -13,35 +13,77 @@ class NsgMetrica {
 
   static void reportEvent(String event, {Map<String, Object>? map}) {
     if (NsgMetrica.instance != null) {
-      if (map == null) {
-        // NsgMetrica.instance!.reportEvent(event);
-      } else {
-        // AppMetrica.reportEventWithMap(event, map);
-      }
+      NsgMetrica.instance!.sendReportEvent(event, map: map);
     }
   }
 
   static void reportAppStart() {
-    // AppMetrica.reportEvent('Application start');
+    if (NsgMetrica.instance != null) {
+      NsgMetrica.instance!.sendReportAppStart();
+    }
   }
 
   static void reportLoginStart(String loginType) {
-    // AppMetrica.reportEventWithMap('Login start', {'loginType': loginType});
+    if (NsgMetrica.instance != null) {
+      NsgMetrica.instance!.sendReportLoginStart(loginType);
+    }
   }
 
   static void reportLoginSuccess(String loginType) {
-    // AppMetrica.reportEventWithMap('Login success', {'loginType': loginType});
+    if (NsgMetrica.instance != null) {
+      NsgMetrica.instance!.sendReportLoginSuccess(loginType);
+    }
   }
 
   static void reportLoginFailed(String loginType, String errorCode) {
-    // AppMetrica.reportEventWithMap('Login failed', {'loginType': loginType, 'errorCode': errorCode});
+    if (NsgMetrica.instance != null) {
+      NsgMetrica.instance!.sendReportLoginFailed(loginType, errorCode);
+    }
   }
 
   static void reportToPage(String pageName) {
-    // AppMetrica.reportEventWithMap('to page', {'pageName': pageName});
+    if (NsgMetrica.instance != null) {
+      NsgMetrica.instance!.sendReportToPage(pageName);
+    }
   }
 
   static void reportTableButtonTap(String tableId, String buttonName, {String state = ''}) {
-    // AppMetrica.reportEventWithMap('table button tap', {'tableId': tableId, 'buttonName': buttonName, 'state': state});
+    if (NsgMetrica.instance != null) {
+      NsgMetrica.instance!.sendReportTableButtonTap(tableId, buttonName, state: state);
+    }
   }
 
+  void sendReportEvent(String event, {Map<String, Object>? map}) {
+    // if (NsgMetrica.instance != null) {
+    //   if (map == null) {
+    //     // NsgMetrica.instance!.reportEvent(event);
+    //   } else {
+    //     // AppMetrica.reportEventWithMap(event, map);
+    //   }
+    // }
+  }
+
+  void sendReportAppStart() {
+    // AppMetrica.reportEvent('Application start');
+  }
+
+  void sendReportLoginStart(String loginType) {
+    // AppMetrica.reportEventWithMap('Login start', {'loginType': loginType});
+  }
+
+  void sendReportLoginSuccess(String loginType) {
+    // AppMetrica.reportEventWithMap('Login success', {'loginType': loginType});
+  }
+
+  void sendReportLoginFailed(String loginType, String errorCode) {
+    // AppMetrica.reportEventWithMap('Login failed', {'loginType': loginType, 'errorCode': errorCode});
+  }
+
+  void sendReportToPage(String pageName) {
+    // AppMetrica.reportEventWithMap('to page', {'pageName': pageName});
+  }
+
+  void sendReportTableButtonTap(String tableId, String buttonName, {String state = ''}) {
+    // AppMetrica.reportEventWithMap('table button tap', {'tableId': tableId, 'buttonName': buttonName, 'state': state});
+  }
+}
