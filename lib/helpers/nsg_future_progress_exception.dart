d953107310d4732793c0a01c0b2d61b3bf5e7046
AndyProtech <andy.protech@gmail.com>
2024-02-27 00:43:55 +0300

    fix

--- lib/helpers/nsg_future_progress_exception.dart
+++ lib/helpers/nsg_future_progress_exception.dart
@@ -3,8 +3,8 @@ import 'package:nsg_controls/nsg_controls.dart';
 import 'package:nsg_controls/widgets/nsg_error_widget.dart';
 
 /// Вызов Future функции с прогресс индикатором и проверкой на ошибки
-Future nsgFutureProgressAndException({required Function() func, String? text, BuildContext? context, bool showProgress = true}) async {
-  var progress = NsgProgressDialog(textDialog: text ?? '', context: context);
+Future nsgFutureProgressAndException({required Function() func, String? text, BuildContext? context, bool showProgress = true, int? delay}) async {
+  var progress = NsgProgressDialog(textDialog: text ?? '', context: context, delay: delay);
   try {
     if (showProgress) {
       progress.show();
