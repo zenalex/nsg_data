3e92bf29fd5938da945c2b44da462a63cf667af5
pro5 <pro5@SRV-FRZ-WEB>
2025-02-06 21:19:02 +0300

    NsgDataDateField - useDate, useTime

--- lib/dataFields/dateField.dart
+++ lib/dataFields/dateField.dart
@@ -5,24 +5,37 @@ import 'package:jiffy/jiffy.dart';
 import '../nsg_data.dart';
 
 class NsgDataDateField extends NsgDataField {
-  NsgDataDateField(String name) : super(name);
+  NsgDataDateField(String name, {this.useDate = true, this.useTime = true}) : super(name);
   @override
   dynamic get defaultValue => DateTime(1);
 
+  bool useDate = true;
+  bool useTime = true;
+
   @override
   dynamic convertJsonValue(dynamic jsonValue) {
-    return DateTime.parse(jsonValue.toString()).toLocal();
+    if (useDate == useTime) return DateTime.parse(jsonValue.toString()).toLocal();
+    return DateTime.parse(jsonValue.toString());
   }
 
   @override
   dynamic convertToJson(dynamic jsonValue) {
-    return (jsonValue as DateTime).toUtc().toIso8601String();
+    if (useDate == useTime) return (jsonValue as DateTime).toUtc().toIso8601String();
+    return (jsonValue as DateTime).toIso8601String();
   }
 
   @override
   void setValue(NsgFieldValues fieldValues, dynamic value) {
     if (value is String) {
-      fieldValues.fields[name] = DateTime.parse(value).toLocal();
+      if (useDate == useTime) {
+        fieldValues.fields[name] = DateTime.parse(value).toLocal();
+      } else {
+        if (value.endsWith('Z') || value.endsWith('z')) {
+          fieldValues.fields[name] = DateTime.parse(value);
+        } else {
+          fieldValues.fields[name] = DateTime.parse(value + 'Z');
+        }
+      }
     } else {
       fieldValues.fields[name] = value;
     }
