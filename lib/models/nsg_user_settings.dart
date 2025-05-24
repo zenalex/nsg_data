0d6c55aa054f30df010f0e574a7fa7d5279c830d
Aleksei <zenkov@nsgsoft.ru>
2025-05-24 11:49:42 +0300

    NsgSettings. add set userId

--- lib/models/nsg_user_settings.dart
+++ lib/models/nsg_user_settings.dart
@@ -3,6 +3,7 @@ import 'package:nsg_data/nsg_data.dart';
 mixin NsgUserSettings {
   String get id;
   String get userId;
+  set userId(String value);
   String get name;
   set name(String value);
   String get settings;
