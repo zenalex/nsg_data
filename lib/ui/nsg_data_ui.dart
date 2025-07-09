1c4be7311291cf583dba8cfee704bf59a0df7568
Mishasik <mishelzenkov@gmail.com>
2025-07-09 14:03:58 +0300

    Update nsg_data_ui.dart

--- lib/ui/nsg_data_ui.dart
+++ lib/ui/nsg_data_ui.dart
@@ -7,9 +7,9 @@ import 'package:nsg_data/ui/nsg_loading_scroll_controller.dart';
 mixin NsgDataUI<T extends NsgDataItem> on NsgDataController<T> {
   int loadStepCountUi = 25;
 
-  Future<List<T>> _loadItems(int top, int count) async {
+  Future<List<T>> _loadItems(int top, int count, {NsgDataRequestParams? filter}) async {
     var matches = NsgDataRequest<T>(dataItemType: T);
-    var filter = getRequestFilter;
+    filter ??= getRequestFilter;
     filter.top = top;
     filter.count = count;
     List<T> ans = await matches.requestItems(filter: filter, loadReference: referenceList);
@@ -23,11 +23,11 @@ mixin NsgDataUI<T extends NsgDataItem> on NsgDataController<T> {
     return filter;
   }
 
-  Future loadNext() async {
+  Future loadNext({NsgDataRequestParams? filter}) async {
     status = GetStatus.loading();
     sendNotify();
     if (items.length + 1 < (totalCount ?? 1000)) {
-      items.addAll(await _loadItems(items.length + 1, loadStepCountUi));
+      items.addAll(await _loadItems(items.length + 1, loadStepCountUi, filter: filter));
     }
     status = GetStatus.success(NsgBaseController.emptyData);
     sendNotify();
