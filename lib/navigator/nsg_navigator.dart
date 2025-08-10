76398344b984924cefbf39fe6ab3f6f8564bd53e
Mishasik <mishelzenkov@gmail.com>
2025-08-10 16:27:18 +0300

    Update nsg_navigator.dart

--- lib/navigator/nsg_navigator.dart
+++ lib/navigator/nsg_navigator.dart
@@ -3,6 +3,14 @@ import 'package:get/get.dart';
 class NsgNavigator {
   static NsgNavigator instance = NsgNavigator();
 
+  static String? initialRoute;
+  static bool useSplashPage = true;
+
+  static bool get isLastPage => (useSplashPage && previousRoute == (initialRoute ?? "/")) || currentRoute == (initialRoute ?? "/");
+
+  static String get currentRoute => Get.currentRoute;
+  static String get previousRoute => Get.previousRoute;
+
   static Future go(String pageName, {String? id, String? widgetId}) async {
     await instance.offAndToPage(pageName, id: id, widgetId: widgetId);
   }
@@ -11,8 +19,8 @@ class NsgNavigator {
     await instance.toPage(pageName, id: id, widgetId: widgetId);
   }
 
-  static void pop() {
-    instance.back();
+  static void pop({String? routeIfLast, void Function()? actionIfLast}) {
+    instance.back(routeIfLast: routeIfLast, actionIfLast: actionIfLast);
   }
 
   Future toPage(String pageName, {String? id, String? widgetId}) async {
@@ -37,7 +45,16 @@ class NsgNavigator {
     await Get.offAndToNamed(pageName, parameters: arg);
   }
 
-  void back() {
+  void back({String? routeIfLast, void Function()? actionIfLast}) {
+    if (isLastPage) {
+      if (routeIfLast != null) {
+        go(routeIfLast);
+      }
+      if (actionIfLast != null) {
+        actionIfLast();
+      }
+      return;
+    }
     Get.back();
   }
 }
