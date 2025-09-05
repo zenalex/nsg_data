8dafa1b03aa9a4a449c65779cc0484adf6ec4f21
Aleksei <zenkov@nsgsoft.ru>
2025-09-06 02:14:45 +0300

    add compare.toString()

--- lib/nsg_data_compare.dart
+++ lib/nsg_data_compare.dart
@@ -83,6 +83,48 @@ class NsgCompare {
     }
     return r;
   }
+
+  @override
+  String toString() {
+    return _toStringFormatted();
+  }
+
+  String _toStringFormatted([int indentLevel = 0]) {
+    if (isEmpty) return '()';
+
+    var conditions = <String>[];
+    for (var param in paramList) {
+      conditions.add(_formatParam(param, indentLevel + 1));
+    }
+
+    if (conditions.length == 1) {
+      return conditions.first;
+    }
+
+    var indent = '  ' * indentLevel;
+    var operatorStr = logicalOperator == NsgLogicalOperator.and ? '\n${indent}AND ' : '\n${indent}OR ';
+    var result = '(\n${indent}  ${conditions.join(operatorStr)}\n${indent})';
+
+    return result;
+  }
+
+  String _formatParam(NsgCompareParam param, int indentLevel) {
+    if (param.parameterValue is NsgCompare) {
+      // Для вложенных условий типа "compare" не показываем оператор
+      if (param.comparisonOperator == NsgComparisonOperator.compare) {
+        var nested = (param.parameterValue as NsgCompare)._toStringFormatted(indentLevel);
+        return '${param.parameterName} ${nested}';
+      } else {
+        var operatorStr = param._getOperatorSymbol();
+        var nested = (param.parameterValue as NsgCompare)._toStringFormatted(indentLevel);
+        return '${param.parameterName} ${operatorStr} ${nested}';
+      }
+    } else {
+      var operatorStr = param._getOperatorSymbol();
+      var valueStr = param._getValueString();
+      return '${param.parameterName} ${operatorStr} ${valueStr}';
+    }
+  }
 }
 
 class NsgCompareParam {
@@ -210,8 +252,154 @@ class NsgCompareParam {
         return value.compareTo(pv) == -1;
       }
       return (value < pv);
+    } else if (comparisonOperator == NsgComparisonOperator.notContainWords) {
+      var words = parameterValue.toString().split(' ');
+      value = value.toString().toLowerCase();
+      return !words.every(((e) => value.contains(e.toLowerCase())));
+    } else if (comparisonOperator == NsgComparisonOperator.inGroup) {
+      return ((parameterValue as List).contains(value));
+    } else if (comparisonOperator == NsgComparisonOperator.groupsFrom) {
+      return ((parameterValue as List).contains(value));
+    } else if (comparisonOperator == NsgComparisonOperator.notGroupsFrom) {
+      return !((parameterValue as List).contains(value));
+    } else if (comparisonOperator == NsgComparisonOperator.equalOrEmpty) {
+      var pv = parameterValue;
+      if (parameterValue is NsgEnum) {
+        pv = (parameterValue as NsgEnum).value;
+      } else if (parameterValue is NsgDataItem) {
+        pv = (parameterValue as NsgDataItem).id;
+      }
+      return (value == pv || value == null || value == '');
+    } else if (comparisonOperator == NsgComparisonOperator.notInList) {
+      return !((parameterValue as List).contains(value));
+    } else if (comparisonOperator == NsgComparisonOperator.notBeginWith) {
+      return !(value.toString().toLowerCase().startsWith(parameterValue.toString().toLowerCase()));
+    } else if (comparisonOperator == NsgComparisonOperator.notEndWith) {
+      return !(value.toString().toLowerCase().endsWith(parameterValue.toString().toLowerCase()));
+    } else if (comparisonOperator == NsgComparisonOperator.notContain) {
+      return !(value.toString().toLowerCase().contains(parameterValue.toString().toLowerCase()));
+    } else if (comparisonOperator == NsgComparisonOperator.notInGroup) {
+      return !((parameterValue as List).contains(value));
+    } else if (comparisonOperator == NsgComparisonOperator.notEqualOrEmpty) {
+      var pv = parameterValue;
+      if (parameterValue is NsgEnum) {
+        pv = (parameterValue as NsgEnum).value;
+      } else if (parameterValue is NsgDataItem) {
+        pv = (parameterValue as NsgDataItem).id;
+      }
+      return (value != pv && value != null && value != '');
+    } else if (comparisonOperator == NsgComparisonOperator.typeIn) {
+      return (parameterValue as List).contains(value.runtimeType);
     } else {
       return false;
+    }
+  }
+
+  @override
+  String toString() {
+    return toStringFormatted(0);
+  }
+
+  String toStringFormatted(int indentLevel) {
+    if (parameterValue is NsgCompare) {
+      var nested = (parameterValue as NsgCompare)._toStringFormatted(indentLevel);
+      return '$parameterName ${comparisonOperator.toString()} $nested';
+    }
+
+    var operatorStr = _getOperatorSymbol();
+    var valueStr = _getValueString();
+
+    return '$parameterName $operatorStr $valueStr';
+  }
+
+  String _getOperatorSymbol() {
+    switch (comparisonOperator) {
+      case NsgComparisonOperator.equal:
+        return '=';
+      case NsgComparisonOperator.notEqual:
+        return '≠';
+      case NsgComparisonOperator.beginWith:
+        return 'startsWith';
+      case NsgComparisonOperator.contain:
+        return '⊃';
+      case NsgComparisonOperator.containWords:
+        return '⊃ words';
+      case NsgComparisonOperator.endWith:
+        return 'endsWith';
+      case NsgComparisonOperator.inList:
+        return '∈';
+      case NsgComparisonOperator.typeEqual:
+        return 'type =';
+      case NsgComparisonOperator.typeNotEqual:
+        return 'type ≠';
+      case NsgComparisonOperator.greaterOrEqual:
+        return '≥';
+      case NsgComparisonOperator.lessOrEqual:
+        return '≤';
+      case NsgComparisonOperator.greater:
+        return '>';
+      case NsgComparisonOperator.less:
+        return '<';
+      case NsgComparisonOperator.notContainWords:
+        return '⊄ words';
+      case NsgComparisonOperator.inGroup:
+        return '∈ group';
+      case NsgComparisonOperator.groupsFrom:
+        return 'groupsFrom';
+      case NsgComparisonOperator.notGroupsFrom:
+        return '¬groupsFrom';
+      case NsgComparisonOperator.equalOrEmpty:
+        return '= ∅';
+      case NsgComparisonOperator.notInList:
+        return '∉';
+      case NsgComparisonOperator.notBeginWith:
+        return '¬startsWith';
+      case NsgComparisonOperator.notEndWith:
+        return '¬endsWith';
+      case NsgComparisonOperator.notContain:
+        return '⊄';
+      case NsgComparisonOperator.notInGroup:
+        return '∉ group';
+      case NsgComparisonOperator.notEqualOrEmpty:
+        return '≠ ∅';
+      case NsgComparisonOperator.typeIn:
+        return 'type ∈';
+      case NsgComparisonOperator.compare:
+        return '';
+      default:
+        return '=';
+    }
+  }
+
+  String _getValueString() {
+    if (parameterValue is DateTime) {
+      return '"${(parameterValue as DateTime).toIso8601String()}"';
+    } else if (parameterValue is NsgEnum) {
+      return '"${(parameterValue as NsgEnum).name}"';
+    } else if (parameterValue is NsgDataItem) {
+      if (comparisonOperator == NsgComparisonOperator.typeEqual || comparisonOperator == NsgComparisonOperator.typeNotEqual) {
+        return '"${(parameterValue as NsgDataItem).typeName}"';
+      } else {
+        return '"${(parameterValue as NsgDataItem).id}"';
+      }
+    } else if (parameterValue is List) {
+      var list = parameterValue as List;
+      if (list.isEmpty) return '[]';
+
+      if (list.first is NsgEnum) {
+        var values = list.map((e) => '"${(e as NsgEnum).name}"').join(', ');
+        return '[$values]';
+      } else if (list.first is NsgDataItem) {
+        var values = list.map((e) => '"${(e as NsgDataItem).id}"').join(', ');
+        return '[$values]';
+      } else {
+        var values = list.map((e) => '"$e"').join(', ');
+        return '[$values]';
+      }
+    } else if (parameterValue is String) {
+      return '"$parameterValue"';
+    } else {
+      return parameterValue.toString();
     }
   }
 }
